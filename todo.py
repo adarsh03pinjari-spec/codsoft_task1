44c0bbac9f7628bb09e945a6d7973fec24a69273
adarsh03pinjari-spec <adarsh03pinjari@gmail.com>
2025-08-07 17:58:34 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -5,7 +5,7 @@
 tasks = [] # List to store task dictionaries
 
 def display_menu():
-    print("\n=== WELCOME TO ADARSH’S TO-DO LIST APP ===")
+    print("\n=== WELCOME TO ADARSH TO-DO LIST APP ===")
     print("1. Add New Task")
     print("2. View All Tasks")
     print("3. Mark Task as Complete")
@@ -15,15 +15,15 @@ def display_menu():
 def add_new_task():
     task = input("Enter a task to add: ")
     tasks.append({"task": task, "done": False})
-    print(" New task added!")
+    print("New task added!")
 
 def view_tasks():
     if not tasks:
         print("No tasks found.")
         return
-    print("\n Your Current Tasks:")
+    print("\nYour Current Tasks:")
     for i, t in enumerate(tasks):
-        status = "✔️" if t["done"] else 
+        status = "Done" if t["done"] else "Pending"
         print(f"{i + 1}. [{status}] {t['task']}")
 
 def mark_task_done():
@@ -31,14 +31,14 @@ def mark_task_done():
     if not tasks:
         return
     try:
-        num = int(input(" Enter task number to mark as done: "))
+        num = int(input("Enter task number to mark as done: "))
         if 1 <= num <= len(tasks):
             tasks[num - 1]["done"] = True
             print("Task marked as completed!")
         else:
             print("Invalid task number.")
     except ValueError:
-        print(" Please enter a valid number.")
+        print("Please enter a valid number.")
 
 def delete_task():
     view_tasks()
@@ -48,7 +48,7 @@ def delete_task():
         num = int(input("Enter task number to delete: "))
         if 1 <= num <= len(tasks):
             removed = tasks.pop(num - 1)
-            print(f" Deleted: {removed['task']}")
+            print(f"Deleted: {removed['task']}")
         else:
             print("Invalid task number.")
     except ValueError:
@@ -68,8 +68,9 @@ while True:
     elif choice == '4':
         delete_task()
     elif choice == '5':
-        print("Thank you for using Adarsh’s To-Do App. Stay organized!")
+        print("Thank you for using Adarsh To-Do App. Stay organized!")
         break
     else:
         print("Invalid choice. Please select between 1 to 5.")
 
+        
